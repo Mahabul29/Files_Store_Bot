7d616c802c402a3e13a81f811126fc697aacec0d
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-01-14 14:41:06 +0530

    Update web_server.py

--- plugins/web_server.py
+++ plugins/web_server.py
@@ -1,1 +1,13 @@
+from aiohttp import web
 
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("Bot is Running")
+
+async def web_server():
+    app = web.Application()
+    app.add_routes(routes)
+    return app
+  
