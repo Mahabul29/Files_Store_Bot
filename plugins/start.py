eff72e43d0d908a1120caa77c0cda37f8293d502
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-01-15 13:46:11 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -111,17 +111,17 @@ async def start_command(client: Client, message: Message):
                 ]
             ]
         )
-        await message.reply_text(
-            text = START_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
+        await message.reply_photo(
+            photo="https://www.uhdpaper.com/2023/07/genshin-impact-furina-game-4k-161m.html",
+            caption=START_MSG.format(
+                first=message.from_user.first_name,
+                last=message.from_user.last_name or "",
+                username=None if not message.from_user.username else '@' + message.from_user.username,
+                mention=message.from_user.mention,
+                id=message.from_user.id
             ),
-            reply_markup = reply_markup,
-            disable_web_page_preview = True,
-            quote = True
+            reply_markup=reply_markup,
+            quote=False # Set this to False to remove the reply/forward styling
         )
         return
 
