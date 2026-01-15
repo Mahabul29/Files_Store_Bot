3dc63589ce1ffd520bbfe38363795331b7d85dc1
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-01-15 22:55:25 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -5,12 +5,3 @@ routes = web.RouteTableDef()
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
     return web.json_response("Madflix_Bots")
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
