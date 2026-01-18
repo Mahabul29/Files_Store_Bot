08cbc18902fbc481bef29befc4c91665016a0302
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-01-19 01:22:56 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -1,7 +1,18 @@
 from aiohttp import web
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton # Add this import
 
 routes = web.RouteTableDef()
+
+# --- ADD THIS HERE ---
+RECALL_BUTTON = InlineKeyboardMarkup([
+    [
+        InlineKeyboardButton("Click Here", callback_data="refresh_files"),
+        InlineKeyboardButton("Close ✖️", callback_data="close_msg")
+    ]
+])
+# ---------------------
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
     return web.json_response("Madflix_Bots")
+    
