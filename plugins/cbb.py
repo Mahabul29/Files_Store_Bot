49eda424637563414820b82d3d6d44419d2de183
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-01-19 21:16:50 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,9 +1,7 @@
-from pyrogram import __version__, filters
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from bot import Bot
-from config import OWNER_ID
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-
-# Inside plugins/cbb.py
+from config import START_MSG, OWNER_ID
 
 @Bot.on_callback_query()
 async def cb_handler(client: Client, query: CallbackQuery):
@@ -13,22 +11,23 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.message.delete()
         
     elif data == "about":
-        # This edits the message to show "About" info and the new Back button
+        # Personalizes the developer link to @Mahabul201
         await query.message.edit_caption(
             caption=f"<b>Mʏ Nᴀᴍᴇ :</b> <a href='https://t.me/Files_Store9_Bot'>Nᴏᴛʜɪɴɢ</a>\n"
                     f"<b>Sᴇʀᴠᴇʀ :</b> <a href='https://app.koyeb.com/'>Kᴏʏᴇʙ</a>\n"
-                    f"<b>Dᴇᴠᴇʟᴏᴘᴇʀ :</b> <a href='https://t.me/Mahabul201'>Mᴏᴏɴ</a>\n"
+                    f"<b>Dᴇᴠᴇʟᴏᴘᴇʀ :</b> <a href='https://t.me/Mahabul201'>@Mahabul201</a>\n"
                     f"<b>Cʜᴀɴɴᴇʟ :</b> <a href='https://t.me/EvaLinks'>Eᴠᴀ Lɪɴᴋs</a>",
+            disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([
                 [
-                    InlineKeyboardButton("Bᴀᴄᴋ", callback_data="back_to_start"),
+                    InlineKeyboardButton("🔙 Gᴏ Bᴀᴄᴋ", callback_data="back_to_start"),
                     InlineKeyboardButton("Cʟᴏsᴇ ✖️", callback_data="close")
                 ]
             ])
         )
 
     elif data == "back_to_start":
-        # This restores the original message exactly as it appears in start.py
+        # Restores your original greeting message and buttons
         await query.message.edit_caption(
             caption=START_MSG.format(
                 first=query.from_user.first_name,
