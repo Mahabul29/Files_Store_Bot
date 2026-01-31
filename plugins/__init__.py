9745be889e93265acd21c94803612bbb97ef6597
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-01-31 23:30:42 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,1 +1,26 @@
+#Hi My Name Is Moon I am Dot Devloper 
 
+
+
+
+
+
+
+
+
+#API_HASH Your API Hash from my.telegram.org
+#APP_ID Your API ID from my.telegram.org
+#BOT_TOKEN Your bot token from @BotFather
+#OWNER_ID Must enter Your Telegram Id
+#FILE_AUTO_DELETE File auto delete, value in seconds
+#CHANNEL_ID Your Channel ID eg:- -100xxxxxxxx
+#DB_URL Your mongo db url
+#DB_NAME Your mongo db session name
+#ADMINS Optional: A space separated list of user_ids of Admins, they can only create links
+#START_MESSAGE Optional: start message of bot, use HTML and fillings
+#FORCE_SUB_MESSAGEOptional:Force sub message of bot, use HTML and Fillings
+#FORCE_SUB_CHANNEL Optional: ForceSub Channel ID, leave 0 if you want disable force sub
+#FORCE_SUB_CHANNEL_2 Optional: ForceSub Channel ID, leave 0 if you want disable force sub
+#PROTECT_CONTENT Optional: True if you need to prevent files from forwarding
+#FORCE_PIC Set Your Force Sub Picture
+#START_PIC Set Your Start massage Picture
