d57352c21f1f646aefd4cff90c04bcdca0ef692f
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-02-09 16:30:50 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,28 +1,32 @@
-Import pymongo, os
+import motor.motor_asyncio  # Use motor for async support
+import os
 from config import DB_URL, DB_NAME
 
-dbclient = pymongo.MongoClient(DB_URL)
+# Initialize the Async MongoDB Client
+dbclient = motor.motor_asyncio.AsyncIOMotorClient(DB_URL)
 database = dbclient[DB_NAME]
 user_data = database['users']
 
-
-
-async def present_user(user_id : int):
-    found = user_data.find_one({'_id': user_id})
+async def present_user(user_id: int):
+    """Check if a user exists in the database."""
+    found = await user_data.find_one({'_id': user_id})
     return bool(found)
 
 async def add_user(user_id: int):
-    user_data.insert_one({'_id': user_id})
+    """Add a new user if they don't already exist."""
+    if not await present_user(user_id):
+        await user_data.insert_one({'_id': user_id})
     return
 
 async def full_userbase():
-    user_docs = user_data.find()
+    """Retrieve all user IDs from the database."""
     user_ids = []
-    for doc in user_docs:
+    async for doc in user_data.find():
         user_ids.append(doc['_id'])
-        
     return user_ids
 
 async def del_user(user_id: int):
-    user_data.delete_one({'_id': user_id})
+    """Delete a user from the database."""
+    await user_data.delete_one({'_id': user_id})
     return
+    
